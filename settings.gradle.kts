0e12544386ff3091296463e16a46c2e4a706d9e5
Julian Blaschke <julianblaschke@icloud.com>
2026-03-30 15:37:03 +0200

    build(gradle): add root settings and shared build configuration, see #2

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "se2-group-codebase"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":apps:android")
+include(":apps:backend")
