57ae63c2fad04878deed982a85738488470501af
Julian Blaschke <julianblaschke@icloud.com>
2026-04-01 15:27:07 +0200

    build(backend): add Spring Boot backend module dependencies

---
+++ apps/backend/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.kotlin.reflect)
+
+    testImplementation(libs.spring.boot.starter.test)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
