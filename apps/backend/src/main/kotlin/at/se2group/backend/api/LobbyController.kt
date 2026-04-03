1f7a2aecb29afcc1051afe247068f4f2261711a5
Julian Blaschke <julianblaschke@icloud.com>
2026-04-03 16:22:26 +0200

    refactor: change endpoint naming to specification in docs - https://github.com/SE2-Gruppenprojekt/se2-group-codebase/blob/main/docs/lobby.md#recommended-backend-lobby-issue-order

--- apps/backend/src/main/kotlin/at/se2group/backend/api/LobbyController.kt
+++ apps/backend/src/main/kotlin/at/se2group/backend/api/LobbyController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/lobby")
+@RequestMapping("/api/lobbies")
 class LobbyController(
     private val lobbyService: LobbyService
 ) {
