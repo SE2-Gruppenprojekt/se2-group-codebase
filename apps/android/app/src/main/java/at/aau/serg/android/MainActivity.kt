17ed79d744233e4fe8c2ef92b0bcc9e779eec90c
ez-del <erzeber@edu.aau.at>
2026-04-06 18:41:19 +0200

    updated new navigation logic
    + now uses NavPaths to confirm google standards.

--- apps/android/app/src/main/java/at/aau/serg/android/MainActivity.kt
+++ apps/android/app/src/main/java/at/aau/serg/android/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import at.aau.serg.android.ui.screens.home.HomeScreen
-import at.aau.serg.android.ui.screens.leaderboard.LeaderboardScreen
+import androidx.navigation.compose.rememberNavController
+import at.aau.serg.android.navigation.AppNavHost
 import at.aau.serg.android.ui.theme.TempappTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,40 +15,18 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TempappTheme {
+                val navController = rememberNavController()
 
-                var currentScreen by remember { mutableStateOf(Screen.HOME) }
-
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-
-                    when (currentScreen) {
-
-                        Screen.HOME -> HomeScreen(
-                            modifier = Modifier.padding(innerPadding),
-                            onCreateLobby = {
-                                // TODO: Navigate to Create Lobby screen
-                            },
-                            onBrowseLobbies = {
-                                // TODO: Navigate to Browse Lobbies screen
-                            },
-                            onShowLeaderboard = {
-                                currentScreen = Screen.LEADERBOARD
-                            },
-                            onSettings = {
-                                // TODO: Navigate to Settings screen
-                            }
-                        )
-
-                        Screen.LEADERBOARD -> LeaderboardScreen(
-                            modifier = Modifier.padding(innerPadding),
-                            onBack = {
-                                currentScreen = Screen.HOME
-                            }
-                        )
-                    }
+                androidx.compose.material3.Scaffold { innerPadding ->
+                    AppNavHost(
+                        navController = navController,
+                        innerPadding = innerPadding
+                    )
                 }
             }
         }
     }
 }
+
+
+
--- apps/android/app/src/main/java/at/aau/serg/android/navigation/AppNavHost.kt
+++ apps/android/app/src/main/java/at/aau/serg/android/navigation/AppNavHost.kt
@@ -1,32 +1,72 @@
 package at.aau.serg.android.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import at.aau.serg.android.ui.screens.home.HomeScreen
 import at.aau.serg.android.ui.screens.leaderboard.LeaderboardScreen
+import at.aau.serg.android.ui.screens.leaderboard.LeaderboardViewModel
 
 @Composable
-fun AppNavHost(navController: NavHostController) {
+fun AppNavHost(
+    navController: NavHostController,
+    innerPadding: PaddingValues = PaddingValues(0.dp)
+) {
     NavHost(
         navController = navController,
-        startDestination = "home"
+        startDestination = "home",
+        route = "root",
+        modifier = Modifier.padding(innerPadding)
     ) {
+
+        // HOME SCREEN
         composable("home") {
+            val parentEntry = remember(navController.currentBackStackEntry) {
+                navController.getBackStackEntry("root")
+            }
+            val leaderboardVM: LeaderboardViewModel = viewModel(parentEntry)
+            val state by leaderboardVM.loadState.collectAsState()
+
             HomeScreen(
+                state = state,
                 onCreateLobby = { navController.navigate("createLobby") },
                 onBrowseLobbies = { navController.navigate("browseLobbies") },
-                onShowLeaderboard = { navController.navigate("leaderboard") },
+                onShowLeaderboard = {
+                    leaderboardVM.loadLeaderboard(
+                        onSuccess = { navController.navigate("leaderboard") },
+                        onError = { }
+                    )
+                },
                 onSettings = { navController.navigate("settings") }
             )
         }
 
+        // LEADERBOARD SCREEN
         composable("leaderboard") {
+            val parentEntry = remember(navController.currentBackStackEntry) {
+                navController.getBackStackEntry("root")
+            }
+            val leaderboardVM: LeaderboardViewModel = viewModel(parentEntry)
+            val players by leaderboardVM.players.collectAsState()
+
             LeaderboardScreen(
+                players = players,
                 onBack = { navController.popBackStack() }
             )
         }
+
+        composable("createLobby") { Text("TODO: Create Lobby Screen") }
+        composable("browseLobbies") { Text("TODO: Browse Lobbies Screen") }
+        composable("settings") { Text("TODO: Settings Screen") }
     }
 }
-
