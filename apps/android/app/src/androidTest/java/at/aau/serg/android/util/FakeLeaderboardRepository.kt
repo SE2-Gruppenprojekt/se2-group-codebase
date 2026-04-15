0b363ad08ef5b72ac15a54808f89d1ba6be9bf95
ez-del <erzeber@edu.aau.at>
2026-04-15 03:41:22 +0200

    refactor(android): removed unused files
    + removed old / unnecessary files planned for testing.

--- apps/android/app/src/androidTest/java/at/aau/serg/android/util/FakeLeaderboardRepository.kt
+++
@@ -1,6 +0,0 @@
-package at.aau.serg.android.util
-
-
-// Handles state and dummy data for leaderboard
-class FakeLeaderboardRepository {
-}
--- apps/android/app/src/androidTest/java/at/aau/serg/android/util/FakeLobbyRepository.kt
+++
@@ -1,5 +0,0 @@
-package at.aau.serg.android.util
-
-// Handles state and dummy data for lobby
-class FakeLobbyRepository {
-}
