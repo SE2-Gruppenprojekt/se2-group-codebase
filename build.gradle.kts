b8be18c1b6af175910321439692b3f89cfb2e270
Julian Blaschke <julianblaschke@icloud.com>
2026-04-21 20:46:00 +0200

    build(sonarqube): update build gradle file to include org.sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
+plugins {
+    id("org.sonarqube") version "7.2.3.7755"
+}
+
 allprojects {
     group = "at.se2group"
     version = "0.1.0"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SE2-Gruppenprojekt_se2-group-codebase")
+        property("sonar.organization", "se2-gruppenprojekt")
+    }
 }
