38808381b2b5f9565e47dc7fe0f621510db779e8
Julian Blaschke <julianblaschke@icloud.com>
2026-04-22 16:06:32 +0200

    build(sonarqube): remove sonar from root gradle file and delegate to apps:backend scope only

--- apps/backend/build.gradle.kts
+++ apps/backend/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
+    id("org.sonarqube")
+    jacoco
 }
 
 java {
@@ -38,4 +40,31 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SE2-Gruppenprojekt_se2-group-codebase")
+        property("sonar.organization", "se2-gruppenprojekt")
+        property("sonar.sources", "src/main/kotlin")
+        property("sonar.tests", "src/test/kotlin")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "build/reports/jacoco/test/jacocoTestReport.xml"
+        )
+    }
 }
