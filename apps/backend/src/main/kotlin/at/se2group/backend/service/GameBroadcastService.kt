02d00fd203357394ae5eb747fb1068bf6201ea22
roldka <kaherold@edu.aau.at>
2026-05-08 21:38:26 +0200

    feat(backend)(game): add draft updated broadcast method

--- apps/backend/src/main/kotlin/at/se2group/backend/service/GameBroadcastService.kt
+++ apps/backend/src/main/kotlin/at/se2group/backend/service/GameBroadcastService.kt
@@ -1,7 +1,10 @@
 package at.se2group.backend.service
 
+import at.se2group.backend.mapper.toResponse
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
+import shared.models.game.domain.TurnDraft
+import shared.models.game.event.GameDraftUpdatedEvent
 
 @Service
 class GameBroadcastService(
@@ -13,5 +16,14 @@ class GameBroadcastService(
 
     private fun gameTopic(gameId: String) = "$GAME_TOPIC_PREFIX/$gameId"
 
-
+    fun broadcastDraftUpdated(draft: TurnDraft) {
+        messagingTemplate.convertAndSend(
+            gameTopic(draft.gameId),
+            GameDraftUpdatedEvent(
+                gameId = draft.gameId,
+                playerId = draft.playerUserId,
+                draft = draft.toResponse()
+            )
+        )
+    }
 }
