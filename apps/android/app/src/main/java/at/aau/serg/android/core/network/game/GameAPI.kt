0761639b845b5cb97b326faa399be6f502effdfd
Julian Blaschke <julianblaschke@icloud.com>
2026-05-15 15:46:53 +0200

    ci(frontend,backend)(sonar): dummy commit to re-trigger sonar code analysis checks

--- apps/android/app/src/main/java/at/aau/serg/android/core/network/game/GameAPI.kt
+++ apps/android/app/src/main/java/at/aau/serg/android/core/network/game/GameAPI.kt
@@ -4,6 +4,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+
 import shared.models.game.request.DrawTileRequest
 import shared.models.game.request.EndTurnRequest
 import shared.models.game.request.UpdateDraftRequest
