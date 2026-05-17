e6d804700c73991ccd34f725a090fcdb03e888f9
ez-del <erzeber@edu.aau.at>
2026-05-18 01:16:31 +0200

    feat(android): added Effects and Events

---
+++ apps/android/app/src/main/java/at/aau/serg/android/ui/screens/game/GameUIEvent.kt
@@ -0,0 +1,24 @@
+package at.aau.serg.android.ui.screens.game
+
+import shared.models.game.domain.Tile
+
+sealed interface GameUIEvent {
+    val requiresActivePlayer: Boolean get() = true
+
+    object AddRow : GameUIEvent
+    object DrawTile : GameUIEvent
+    object ResetSelection : GameUIEvent
+    data class MoveTiles(val rowId: String?) : GameUIEvent
+    data class MoveInSameRow(val rowId: String?, val from: Int, val to: Int) : GameUIEvent
+    data class OnTileSelected(val tile: Tile, val selected: Boolean, val rowId: String?) : GameUIEvent
+    object EndTurn : GameUIEvent
+    data class OnLoadGame(val gameId: String) : GameUIEvent {
+        override val requiresActivePlayer = false
+    }
+    data object OnSettings : GameUIEvent {
+        override val requiresActivePlayer = false
+    }
+    data object OnBack : GameUIEvent {
+        override val requiresActivePlayer = false
+    }
+}
