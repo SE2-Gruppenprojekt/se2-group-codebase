9beedc94d1f08d987fbd845fe62b6d1d45023545
miri2468 <manasrehmiriam1016@gmail.com>
2026-06-12 21:03:01 +0200

    feat(shared)(lobby): add requireInitialMeld to lobby request DTO

--- apps/shared/src/main/kotlin/shared/models/lobby/request/CreateLobbyRequest.kt
+++ apps/shared/src/main/kotlin/shared/models/lobby/request/CreateLobbyRequest.kt
@@ -4,5 +4,6 @@ data class CreateLobbyRequest(
     val displayName: String,
     val maxPlayers: Int = 4,
     val isPrivate: Boolean = false,
-    val allowGuests: Boolean = true
+    val allowGuests: Boolean = true,
+    val requireInitialMeld: Boolean = true
 )
