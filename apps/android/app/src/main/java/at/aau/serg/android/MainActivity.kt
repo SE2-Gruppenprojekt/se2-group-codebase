a3b953d4c3a58c3ea8b46fdd790b404011753625
Julian Blaschke <julianblaschke@icloud.com>
2026-06-12 21:37:58 +0200

    feat(android)(security)(jwt,session,auth): reset local session on unauthorized bearer auth failures

--- apps/android/app/src/main/java/at/aau/serg/android/MainActivity.kt
+++ apps/android/app/src/main/java/at/aau/serg/android/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.remember
 import androidx.navigation.compose.rememberNavController
 import at.aau.serg.android.core.datastore.DataStoreProvider
-import at.aau.serg.android.core.datastore.getStore
-import at.aau.serg.android.datastore.proto.User
+import at.aau.serg.android.core.datastore.user.UserStore
+import at.aau.serg.android.core.network.ServiceLocator
 import at.aau.serg.android.ui.navigation.AppNavHost
 import at.aau.serg.android.ui.theme.TempappTheme
 import at.aau.serg.android.ui.theme.ThemeState
@@ -18,6 +18,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        val provider = DataStoreProvider.getInstance(this)
+        val userStore = provider.getStore(at.aau.serg.android.datastore.proto.User::class) as UserStore
+        ServiceLocator.initialize(userStore)
+
         setContent {
 
             TempappTheme(
@@ -26,15 +30,15 @@ class MainActivity : ComponentActivity() {
 
                 val navController = rememberNavController()
 
-                val provider = remember { DataStoreProvider.getInstance(this) }
-                val userStore = remember { provider.getStore<User>() }
+                val rememberedProvider = remember { provider }
+                val rememberedUserStore = remember { userStore }
 
                 androidx.compose.material3.Scaffold { innerPadding ->
                     AppNavHost(
                         navController = navController,
                         context = this@MainActivity,
                         innerPadding = innerPadding,
-                        userStore = userStore,
+                        userStore = rememberedUserStore,
                     )
                 }
             }
